2df22f52b4a83d46250ded2bc9908f220664382c
Ramjivan Jangid <ramjeewanj@gmail.com>
2021-08-25 19:56:12 +0530

    setup

--- chk copy done 25-08-2021/setup.sh
+++ chk copy done 25-08-2021/setup.sh
@@ -6,9 +6,14 @@ sudo usermod -aG  sudo pi
 #making bash files executable
 chmod +x copy.sh
 chmod +x cpo.sh
-#chmod +x copier.py
+chmod +x copier.py
+chmod +x /etc/rc.local
 
-sudo echo 'python copier.py' >> .
+# startup script | adding to rc.local
+sed '$i\
+python copier.py &' ~/tmp | tee ~/tmp
+echo "$(<~/tmp )" > /etc/rc.local
+rm ~/tmp
 
 sudo echo 'done' > ~/copy_completed.txt
 
