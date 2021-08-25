6f91fbd34030309e73172e8d080d166b57d529e6
Ramjivan Jangid <ramjeewanj@gmail.com>
2021-08-25 22:25:54 +0530

    setup file fix

--- chk copy done 25-08-2021/setup.sh
+++ chk copy done 25-08-2021/setup.sh
@@ -9,11 +9,14 @@ chmod +x cpo.sh
 chmod +x copier.py
 chmod +x /etc/rc.local
 
-# startup script | adding to rc.local
-sed '$i\
-python copier.py &' ~/tmp | tee ~/tmp
-echo "$(<~/tmp )" > /etc/rc.local
-rm ~/tmp
-
 sudo echo 'done' > ~/copy_completed.txt
+
+# startup script | adding to rc.local
+sudo echo '#!/bin/sh -e
+
+python3 /home/pi/copier.py &
+
+exit 0' > /etc/rc.local
+
+
 
